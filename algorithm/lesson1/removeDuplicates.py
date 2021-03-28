458c5118098d1feb420694d911e8ff6138d23f07
liubin <bin.liu@nexttao.com>
2021-03-28 20:54:08 +0800

    removeDuplicates

---
+++ algorithm/lesson1/removeDuplicates.py
@@ -0,0 +1,22 @@
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        p = 0
+        q = 1
+        while (q < len(nums)):
+            if nums[p] != nums[q]:
+                nums[p + 1] = nums[q]
+                p+=1
+            q+=1
+        return nums[:p + 1]
+
+
+if __name__ == '__main__':
+    result = Solution().removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+    print(result)
